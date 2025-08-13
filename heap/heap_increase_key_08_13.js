66af854c4af652ee8eb849353fb9784e23ceecd3
CalvinZhang1013 <76543159+CalvinZhang1013@users.noreply.github.com>
2025-08-13 09:37:35 +0800

    add heap increase heap

---
+++ heap/heap_increase_key_08_13.js
@@ -0,0 +1,44 @@
+const a = [5, 3, 17, 10, 84, 19, 6, 22, 9];
+
+function heap_increase_key(A, i, key) {
+  build_max_heap(A);
+  if (key < A[i]) {
+    throw "key is smaller than i";
+  }
+  A[i] = key;
+  parent = Math.floor((i - 1) / 2);
+  while (i > 0 && A[parent] < A[i]) {
+    [A[i], A[parent]] = [A[parent], A[i]];
+    i = Math.floor((parent - 1) / 2);
+  }
+}
+
+function build_max_heap(numbers) {
+  let length = numbers.length;
+  for (let i = length - 1; i >= 0; i--) {
+    max_heapify(numbers, i);
+  }
+}
+
+function max_heapify(numbers, i) {
+  let left = 2 * i + 1;
+  let right = 2 * i + 2;
+
+  if (left <= numbers.length && numbers[left] > numbers[i]) {
+    largest = left;
+  } else {
+    largest = i;
+  }
+  if (right <= numbers.length && numbers[right] > numbers[largest]) {
+    largest = right;
+  }
+  if (largest != i) {
+    let temp = numbers[largest];
+    numbers[largest] = numbers[i];
+    numbers[i] = temp;
+    max_heapify(numbers, largest);
+  }
+}
+
+heap_increase_key(a, 1, 23);
+console.log(a);
